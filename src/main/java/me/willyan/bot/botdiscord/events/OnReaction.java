bad78e1df56390ec356925b5404d4bced2bf50fa
Willyan <willyanf17@hotmail.com>
2022-02-08 22:07:36 -0300

    bug fix in system of ideas

--- src/main/java/me/willyan/bot/botdiscord/events/OnReaction.java
+++ src/main/java/me/willyan/bot/botdiscord/events/OnReaction.java
@@ -3,10 +3,7 @@ package me.willyan.bot.botdiscord.events;
 import me.willyan.bot.botdiscord.lib.ConfigManager;
 import me.willyan.bot.botdiscord.util.Embed;
 import net.dv8tion.jda.api.Permission;
-import net.dv8tion.jda.api.entities.Category;
-import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.MessageReaction;
-import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
@@ -30,11 +27,9 @@ public class OnReaction extends ListenerAdapter {
         if(reaction.getName().equalsIgnoreCase("✅")) {
             if(e.getTextChannel().getId().equals(ConfigManager.get("channelIdeaForADM"))){
 
-                String msg = e.retrieveMessage().toString();
-                e.getJDA().getTextChannelById(ConfigManager.get("channelSugerir")).sendMessage("> **-----------------------------------------------**").queue();
-                e.getJDA().getTextChannelById(ConfigManager.get("channelSugerir")).sendMessage("> **Sugestão de** " + e.getMember().getAsMention()).queue();
-                e.getJDA().getTextChannelById(ConfigManager.get("channelSugerir")).sendMessage("> "+ msg).queue();
-                e.getJDA().getTextChannelById(ConfigManager.get("channelSugerir")).sendMessage("> **-----------------------------------------------**").complete().addReaction("❤").queue();
+                Message retrivedMessage = e.retrieveMessage().complete();
+                String msg =  retrivedMessage.getContentRaw();
+                e.getJDA().getTextChannelById(ConfigManager.get("channelSugerir")).sendMessage(Embed.create("Nova sugestão para o servidor! " , msg)).complete().addReaction("❤").queue();
             }
         }
 
